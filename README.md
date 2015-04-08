d4d961e5d340e6c1d70cceb42f6a2136d286cc99
John Otander <johnotander@gmail.com>
2015-04-08 15:38:25 -0600

    Update readme, add license and package information

--- README.md
+++ README.md
@@ -2,16 +2,10 @@
 
 Elixir package to check whether a given url is up.
 
-## Installation
-
-```
-
-```
-
 ## Usage
 
 ```elixir
-IsUp.check('https://google.com')
+IsUp.check('https://google.com') # => true
 ```
 
 ## Tests
@@ -20,5 +14,16 @@ IsUp.check('https://google.com')
 mix test
 ```
 
+## License
+
+MIT
+
 ## Contributing
 
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+Crafted with <3 by John Otander ([@4lpine](https://twitter.com/4lpine)).
--- mix.exs
+++ mix.exs
@@ -2,12 +2,19 @@ defmodule IsUp.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :is_up,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :is_up,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      package: [
+        contributors: ["John Otander"],
+        licenses: ["MIT"],
+        links: %{ "Github" => "https://github.com/johnotander/is_up" }
+      ]
+    ]
   end
 
   # Configuration for the OTP application
