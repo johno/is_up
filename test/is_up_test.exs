7d4d94dd516216c520aa214f5577d14a5d30d634
John Otander <johnotander@gmail.com>
2015-04-09 10:37:24 -0600

    Add more thorough success tests

--- test/is_up_test.exs
+++ test/is_up_test.exs
@@ -2,7 +2,7 @@ defmodule IsUpTest do
   use ExUnit.Case
 
   test "it returns true for an available site" do
-    assert IsUp.check("http://httbin.org/get")
+    for url <- ["http://httpbin.org/get", "https://www.google.com/", "http://furtive.co"], do: assert(IsUp.check(url))
   end
 
   test "it returns false for a site that is not up" do
